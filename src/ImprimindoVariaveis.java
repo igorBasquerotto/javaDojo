bda2d54fe79141849c96bc5072c24b9c729933e1
igorBasquerotto <igor_bas2005@hotmail.com>
2019-11-11 14:28:19 -0300

    Aulas 1 a 7

---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,9 @@
+public class ImprimindoVariaveis {
+    public static void main(String[] args) {
+        String nome = "Igor Basquerotto";
+        int idade = 27;
+
+        System.out.println();
+
+    }
+}
---
+++ src/OlaMundo.java
@@ -0,0 +1,5 @@
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo, esse é meu primeiro programa usando GIT");
+    }
+}
